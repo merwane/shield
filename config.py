bd59d8a26f254a45a10fd21a8c8b780d09d73786
Merwane Drai <merwanedr@gmail.com>
2021-07-31 13:52:54 +0200

    add local support for ShieldPassword methods

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 TEST_MODE = os.getenv("TEST_MODE")
+OPERATING_SYSTEM = platform.system()
 
 REDIS_HOST = "localhost"
 REDIS_PORT = "6379"
@@ -16,7 +17,7 @@ RQ_HOST = "localhost"
 RQ_PORT = "6379"
 
 # Mac
-if platform.system() == "Darwin":
+if OPERATING_SYSTEM == "Darwin":
     if TEST_MODE == "1":
         FILES_PATH = os.getenv('FILES_PATH')
         DISK_PATH = os.getenv('DISK_PATH')
@@ -25,7 +26,7 @@ if platform.system() == "Darwin":
         FILES_PATH = "/extdisk/files"
         DISK_PATH = "/extdisk/files"
         API_URL = "http://shield.local"
-elif platform.system() == "Linux":
+elif OPERATING_SYSTEM == "Linux":
     # RPi
     FILES_PATH = "/extdisk/files"
     DISK_PATH = "/extdisk/files"
--- encryption/hash_pass.py
+++ encryption/hash_pass.py
@@ -1,15 +1,22 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from config import OPERATING_SYSTEM
 import os
 
 class ShieldPassword:
     def __init__(self):
         self.password_hash = ""
         # check if password hash file exists
-        is_file = os.path.isfile("/keys/hashed_password.key")
+        if OPERATING_SYSTEM == "Linux":
+            is_file = os.path.isfile("/keys/hashed_password.key")
+        else:
+            is_file = os.path.isfile("./hashed_password.key")
         self.is_file = is_file
 
         if is_file:
-            self.password_hash = open("/keys/hashed_password.key", "rb").read()
+            if OPERATING_SYSTEM == "Linux":
+                self.password_hash = open("/keys/hashed_password.key", "rb").read()
+            else:
+                self.password_hash = open("./hashed_password.key", "rb").read()
         else:
             pass
 
@@ -19,9 +26,14 @@ class ShieldPassword:
         return self.password_hash
 
     def save_password(self):
-        with open("/keys/hashed_password.key", "wb") as password_file:
-                password_hash = self.password_hash.encode()
-                password_file.write(password_hash)
+        if OPERATING_SYSTEM == "Linux":
+            with open("/keys/hashed_password.key", "wb") as password_file:
+                    password_hash = self.password_hash.encode()
+                    password_file.write(password_hash)
+        else:
+            with open("./hashed_password.key", "wb") as password_file:
+                    password_hash = self.password_hash.encode()
+                    password_file.write(password_hash)
 
     def check_password(self, password):
         hash_match = check_password_hash(self.password_hash.decode("utf-8"), password)
