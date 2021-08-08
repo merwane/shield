6a54cf009fda18cda2a5e9f8af21a9d9e8bb6edf
Merwane Drai <merwanedr@gmail.com>
2021-08-08 17:09:57 +0200

    refactor upload function

--- api/controllers/upload.py
+++ api/controllers/upload.py
@@ -1,18 +1,7 @@
 from flask_restful import Resource, reqparse
-from encryption.encrypt import encrypt_file
-from services.queue_config import JobQueue
-from config import FILES_PATH
-from api.resources.files.database_operations import add_file
-from file_handler.info import File
+from api.resources.files.upload import upload_files
 from flask import request
 import werkzeug
-import uuid
-import os
-from file_handler.utils import bytesto
-
-# job queue
-job_queue = JobQueue('default')
-queue = job_queue.queue()
 
 class UploadFile(Resource):
     def post(self):
@@ -24,24 +13,7 @@ class UploadFile(Resource):
                 action='append')
         args = parse.parse_args()
         all_files = args.get('file')
-        filenames = []
-        for f in all_files:
-            file_extension = os.path.splitext(f.filename)[1]
-            file_uid = uuid.uuid4().hex
-            filename = "{}{}".format(file_uid, file_extension)
-            file_path = "{}/{}".format(FILES_PATH, filename)
 
-            # Write file to disk
-            f.save(file_path)
-            
-            # get file size
-            file_size = os.path.getsize(file_path)
-            file_size = bytesto(file_size, 'm')
-
-            # Add file to Files database
-            file_type = file_extension.replace(".", "")
-            queue.enqueue(add_file, filename, file_size, file_type, result_ttl=0)
-
-            filenames.append(filename)
+        filenames = upload_files(all_files)
 
         return filenames
---
+++ api/resources/files/upload.py
@@ -0,0 +1,33 @@
+from api.resources.files.database_operations import add_file
+from services.queue_config import JobQueue
+from config import FILES_PATH
+import uuid
+import os
+from file_handler.utils import bytesto
+
+# job queue
+job_queue = JobQueue('default')
+queue = job_queue.queue()
+
+def upload_files(all_files):
+    filenames = []
+    for f in all_files:
+        file_extension = os.path.splitext(f.filename)[1]
+        file_uid = uuid.uuid4().hex
+        filename = "{}{}".format(file_uid, file_extension)
+        file_path = "{}/{}".format(FILES_PATH, filename)
+
+        # Write file to disk
+        f.save(file_path)
+            
+        # get file size
+        file_size = os.path.getsize(file_path)
+        file_size = bytesto(file_size, 'm')
+
+        # Add file to Files database
+        file_type = file_extension.replace(".", "")
+        queue.enqueue(add_file, filename, file_size, file_type, result_ttl=0)
+
+        filenames.append(filename)
+    
+    return filenames
