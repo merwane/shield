cca3333b51fa362836cfe1e8dd30dfcfbf8bfb61
Merwane Drai <merwanedr@gmail.com>
2021-08-08 18:04:22 +0200

    remove cryptography package

--- api/resources/files/download.py
+++ api/resources/files/download.py
@@ -1,22 +1,7 @@
-from encryption.decrypt import decrypt_file
 from shutil import copyfile
 from config import FILES_PATH
 from flask_restful import abort
 import os
-
-"""
-def decrypt_and_download(filename, key):
-    # copy file to temporary folder
-    try:
-        copyfile("{}/{}".format(FILES_PATH, filename), "/tmp/{}".format(filename))
-    except FileNotFoundError:
-        abort(404, error="file not found") 
-
-    # decrypt copied file
-    name = decrypt_file(filename, key, tmp=True)
-
-    return name
-"""
 
 def get_file_full_path(filename):
     filename = "{}/{}".format(FILES_PATH, filename)
