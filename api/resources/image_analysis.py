558d2a0da827161dc50cd0e922220ce0ed34cf9f
Merwane Drai <merwanedr@gmail.com>
2021-08-12 10:30:24 +0200

    get rid of TF and dependencies

--- api/resources/image_analysis.py
+++ api/resources/image_analysis.py
@@ -1,30 +1,11 @@
-from config import OPERATING_SYSTEM
-import os
-
-if OPERATING_SYSTEM == "Linux":
-    from imageai.Classification import ImageClassification
-    
-    execution_path = os.getcwd()
-    model_path = "{}/model/{}".format(execution_path, "resnet.h5")
-    prediction = ImageClassification()
-    prediction.setModelTypeAsResNet50()
-    prediction.setModelPath(model_path)
-    prediction.loadModel()
-
-else:
-    print("--- Classification not implemented in Darwin test mode ---")
-
 def classify(image_path):
     labels = []
 
-    if OPERATING_SYSTEM == "Linux":
-        predictions, probabilities = prediction.classifyImage(image_path, result_count=5)
-        for index in range(len(predictions)):
-            if probabilities[index] > 10:
-                labels.append(predictions[index])
-            else:
-                pass
-    elif OPERATING_SYSTEM == "Darwin":
-        pass
+    predictions = [] # TODO: fill
+    for index in range(len(predictions)):
+        if probabilities[index] > 10:
+            labels.append(predictions[index])
+        else:
+            pass
         
     return labels
