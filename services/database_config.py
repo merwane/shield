faae1cf840f34e9ef1037704bfeadcfa8cd1b018
Merwane Drai <merwanedr@gmail.com>
2021-08-15 14:03:59 +0200

    add TLS certificates for mongo connection

--- services/database_config.py
+++ services/database_config.py
@@ -1,14 +1,15 @@
 import pymongo
 from config import DATABASE_HOST, DATABASE_CLIENT
 from mongoengine import connect
+import certifi
 
 class Database:
     def __init__(self):
         self.host = DATABASE_HOST
         self.client = DATABASE_CLIENT
-        connect(DATABASE_CLIENT, host=DATABASE_HOST, retryWrites=False, tls=True, tlsAllowInvalidCertificates=True)
+        connect(DATABASE_CLIENT, host=DATABASE_HOST, retryWrites=False, tls=True, tlsAllowInvalidCertificates=True, tlsCAFile=certifi.where())
 
     def light(self):
-        client = pymongo.MongoClient(host=self.host, retryWrites=False, tls=True, tlsAllowInvalidCertificates=True)
+        client = pymongo.MongoClient(host=self.host, retryWrites=False, tls=True, tlsAllowInvalidCertificates=True, tlsCAFile=certifi.where())
         database = client[self.client]
         return database
