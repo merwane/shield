b811079074dc32156606eb453a80654d273d2f1a
Merwane Drai <merwanedr@gmail.com>
2021-08-15 14:36:30 +0200

    revert SSL parameter mongo

--- services/database_config.py
+++ services/database_config.py
@@ -7,9 +7,9 @@ class Database:
     def __init__(self):
         self.host = DATABASE_HOST
         self.client = DATABASE_CLIENT
-        connect(DATABASE_CLIENT, host=DATABASE_HOST, retryWrites=False, tls=True, tlsAllowInvalidCertificates=True, sslAllowInvalidCertificates=True)
+        connect(DATABASE_CLIENT, host=DATABASE_HOST, retryWrites=False, tls=True, tlsAllowInvalidCertificates=True)
 
     def light(self):
-        client = pymongo.MongoClient(host=self.host, retryWrites=False, tls=True, tlsAllowInvalidCertificates=True, sslAllowInvalidCertificates=True)
+        client = pymongo.MongoClient(host=self.host, retryWrites=False, tls=True, tlsAllowInvalidCertificates=True)
         database = client[self.client]
         return database
