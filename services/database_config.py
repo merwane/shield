f62bc237a5e66479c4b5f3d2de5974401b247828
Merwane Drai <merwanedr@gmail.com>
2021-08-15 15:10:21 +0200

    yet another attempt to fix mongo SSL

--- services/database_config.py
+++ services/database_config.py
@@ -7,9 +7,9 @@ class Database:
     def __init__(self):
         self.host = DATABASE_HOST
         self.client = DATABASE_CLIENT
-        connect(DATABASE_CLIENT, host=DATABASE_HOST, retryWrites=False, tls=True, tlsAllowInvalidCertificates=True)
+        connect(DATABASE_CLIENT, host=DATABASE_HOST, retryWrites=False, tls=True, tlsAllowInvalidCertificates=True, ssl_cert_reqs=ssl.CERT_NONE)
 
     def light(self):
-        client = pymongo.MongoClient(host=self.host, retryWrites=False, tls=True, tlsAllowInvalidCertificates=True)
+        client = pymongo.MongoClient(host=self.host, retryWrites=False, tls=True, tlsAllowInvalidCertificates=True, ssl_cert_reqs=ssl.CERT_NONE)
         database = client[self.client]
         return database
