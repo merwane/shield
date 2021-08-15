98ef1415137cd2bba1c181804e63e7b769111d90
Merwane Drai <merwanedr@gmail.com>
2021-08-15 15:28:18 +0200

    disable TLS

--- services/database_config.py
+++ services/database_config.py
@@ -1,15 +1,14 @@
 import pymongo
 from config import DATABASE_HOST, DATABASE_CLIENT
 from mongoengine import connect
-import ssl
 
 class Database:
     def __init__(self):
         self.host = DATABASE_HOST
         self.client = DATABASE_CLIENT
-        connect(DATABASE_CLIENT, host=DATABASE_HOST, retryWrites=False, tls=True, tlsAllowInvalidCertificates=True, ssl_cert_reqs=ssl.CERT_NONE)
+        connect(DATABASE_CLIENT, host=DATABASE_HOST, retryWrites=False, tls=False, tlsAllowInvalidCertificates=True)
 
     def light(self):
-        client = pymongo.MongoClient(host=self.host, retryWrites=False, tls=True, tlsAllowInvalidCertificates=True, ssl_cert_reqs=ssl.CERT_NONE)
+        client = pymongo.MongoClient(host=self.host, retryWrites=False, tls=False, tlsAllowInvalidCertificates=True)
         database = client[self.client]
         return database
