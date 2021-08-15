89d4fe7a11f0185be850532118b99e76579027b0
Merwane Drai <merwanedr@gmail.com>
2021-08-15 14:32:56 +0200

    add sslAllowInvalidCertificates to mongo

--- services/database_config.py
+++ services/database_config.py
@@ -7,9 +7,9 @@ class Database:
     def __init__(self):
         self.host = DATABASE_HOST
         self.client = DATABASE_CLIENT
-        connect(DATABASE_CLIENT, host=DATABASE_HOST, retryWrites=False, tls=True, tlsAllowInvalidCertificates=True, ssl=True,ssl_cert_reqs=ssl.CERT_NONE)
+        connect(DATABASE_CLIENT, host=DATABASE_HOST, retryWrites=False, tls=True, tlsAllowInvalidCertificates=True, sslAllowInvalidCertificates=True)
 
     def light(self):
-        client = pymongo.MongoClient(host=self.host, retryWrites=False, tls=True, tlsAllowInvalidCertificates=True, ssl=True,ssl_cert_reqs=ssl.CERT_NONE)
+        client = pymongo.MongoClient(host=self.host, retryWrites=False, tls=True, tlsAllowInvalidCertificates=True, sslAllowInvalidCertificates=True)
         database = client[self.client]
         return database
