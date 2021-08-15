5231a9267004d90d44a2632c41e2ddc58e2d64ba
Merwane Drai <merwanedr@gmail.com>
2021-08-15 14:17:11 +0200

    SSL bug fix mongo

--- services/database_config.py
+++ services/database_config.py
@@ -1,15 +1,15 @@
 import pymongo
 from config import DATABASE_HOST, DATABASE_CLIENT
 from mongoengine import connect
-import certifi
+import ssl
 
 class Database:
     def __init__(self):
         self.host = DATABASE_HOST
         self.client = DATABASE_CLIENT
-        connect(DATABASE_CLIENT, host=DATABASE_HOST, retryWrites=False, tls=True, tlsAllowInvalidCertificates=True, tlsCAFile=certifi.where())
+        connect(DATABASE_CLIENT, host=DATABASE_HOST, retryWrites=False, tls=True, tlsAllowInvalidCertificates=True, ssl=True,ssl_cert_reqs=ssl.CERT_NONE)
 
     def light(self):
-        client = pymongo.MongoClient(host=self.host, retryWrites=False, tls=True, tlsAllowInvalidCertificates=True, tlsCAFile=certifi.where())
+        client = pymongo.MongoClient(host=self.host, retryWrites=False, tls=True, tlsAllowInvalidCertificates=True, ssl=True,ssl_cert_reqs=ssl.CERT_NONE)
         database = client[self.client]
         return database
